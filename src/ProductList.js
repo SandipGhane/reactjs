016a44097901b3e463dccb07d1027eaaa0ed8866
= <sandip.humation@gmail.com>
2021-02-05 20:14:00 +0530

    feat : set error

--- src/ProductList.js
+++ src/ProductList.js
@@ -1,12 +1,19 @@
+import { useState, useEffect } from 'react';
 import Cars from './Cars';
-const ProductList = () => (
-    <div className="product-list">
+const ProductList = () => {
+    const [count, setCount] = useState(1);
+    if (count == 3) {
+        throw new Error('I crashed!');
+    }
+    return <div className="product-list">
         <h2>Product list:</h2>
+        <button onClick={() => setCount(count + 1)} >Click</button>
         <Cars />
         {/* Other potential product categories which we will skip for this demo: */}
         {/* <Electronics /> */}
         {/* <Clothes /> */}
         {/* <Shoes /> */}
-    </div>
-);
+    </div >
+}
+
 export default ProductList;
